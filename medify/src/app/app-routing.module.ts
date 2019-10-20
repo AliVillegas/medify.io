b8dc6f76e52ffc76d234cf96bb01147b6ab2d851
Julian H <cjulianh46@gmail.com>
2019-10-19 21:34:45 -0500

    Added routing to landing page and login/register forms

--- medify/src/app/app-routing.module.ts
+++ medify/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RegisterDoctorComponent } from './register-doctor/register-doctor.component';
+import { RegisterPatientComponent } from './register-patient/register-patient.component';
+import { LoginPatientComponent } from './login-patient/login-patient.component';
+import { LoginDoctorComponent } from './login-doctor/login-doctor.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: 'landing', component: LandingPageComponent },
+  { path: 'register-patient', component: RegisterPatientComponent },
+  { path: 'register-doctor', component: RegisterDoctorComponent },
+  { path: 'login-patient', component: LoginPatientComponent },
+  { path: 'login-doctor', component: LoginDoctorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
