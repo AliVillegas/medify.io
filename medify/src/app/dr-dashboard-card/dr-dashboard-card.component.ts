891ca6ec997c87c10a20e354ad969aea7a76c87f
alibryan.villegas@gmail.com <alibryan.villegas@gmail.com>
2019-10-22 13:55:28 -0500

    Added DR appoinment model Dr dashboard card ngfor

--- medify/src/app/dr-dashboard-card/dr-dashboard-card.component.ts
+++ medify/src/app/dr-dashboard-card/dr-dashboard-card.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Appointment} from './Appointment'
 @Component({
   selector: 'app-dr-dashboard-card',
   templateUrl: './dr-dashboard-card.component.html',
   styleUrls: ['./dr-dashboard-card.component.scss']
 })
+
+
 export class DrDashboardCardComponent implements OnInit {
+  private appointments: Appointment[];
 
   constructor() { }
 
   ngOnInit() {
+    this.appointments = [
+      new Appointment("Julián Herrera", "Lunes", "15", "Sep", "Revisión Cáncer y Quimioterapia"),
+      new Appointment("Saúl Neri", "Martes", "16", "Sep", "Defícit de Atención en clases"),
+      new Appointment("Ali Villegas", "Viernes", "03", "Oct", "Cita Nutriología")
+    ]
+
   }
 
 }
