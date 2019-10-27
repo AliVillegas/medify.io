3e4e77079e84a6227cbc26dfa4419ddb254787a0
alibryan.villegas@gmail.com <alibryan.villegas@gmail.com>
2019-10-26 22:50:16 -0500

    Citas dinámicas en dashboard de paciente

--- medify/src/app/appointment-dashboard-card/appointment-dashboard-card.component.ts
+++ medify/src/app/appointment-dashboard-card/appointment-dashboard-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserdataService } from '../userdata.service';
+import { Appointment } from '../Models/Appointment';
 
 @Component({
   selector: 'app-appointment-dashboard-card',
@@ -7,23 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentDashboardCardComponent implements OnInit {
 
-  private doctorName: String
-  private place: String
-  private appointmentReason: String
-  private time: String
-  private address: String
-  private date: String
+  private appointments: Appointment[];
 
-  constructor() { }
-
-  ngOnInit() {
-    this.doctorName = "Dra. Torres";
-    this.place = "Consultorio Especializado";
-    this.appointmentReason = "Dolor De cabeza";
-    this.address = "Av. Libertad 22";
-    this.date = "28/10/2019"
-    this.time = "16:30";
+  constructor(private userData: UserdataService) {
   }
+  
+  ngOnInit() {
+    this.userData.currentAppointments.subscribe(appointments => this.appointments = appointments);
+
+  }
+
 
 
 }
