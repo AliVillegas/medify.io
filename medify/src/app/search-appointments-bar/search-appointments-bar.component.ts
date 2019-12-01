86a16d348bc65c0b755b7ddc8d8c480f3d9125be
Julian H <cjulianh46@gmail.com>
2019-12-01 17:54:59 -0600

    Fixed calendar responsive, added search bar in appointments patient, added new intl

--- medify/src/app/search-appointments-bar/search-appointments-bar.component.ts
+++ medify/src/app/search-appointments-bar/search-appointments-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-search-appointments-bar',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchAppointmentsBarComponent implements OnInit {
 
-  constructor() { }
+  searchForm: FormGroup
+  searchField = new FormControl('');
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.searchForm = this.fb.group({
+      title: ''
+    })
+  }
+
+  searchAppointment(query) {
+    var err = document.getElementById("error")
+    var l = localStorage.getItem("language")
+    if (query === '' || query === ' ') {
+      if (l != null) {
+        if (l == "es") {
+          err.innerHTML = "Debes llenar el campo"
+        }
+        else {
+          err.innerHTML = "You must fill in the field"
+        }
+      }
+    }
+    else {
+      err.innerHTML = ""
+    }
   }
 
 }
