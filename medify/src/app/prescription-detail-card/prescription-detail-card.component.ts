7e7e27a516b0083d1b01983338ed75db2ea16315
AllBran <alibryan.villegas@gmail.com>
2019-12-02 09:44:56 -0600

    Finished Prescriptions Delivered change on loopback & mongo

--- medify/src/app/prescription-detail-card/prescription-detail-card.component.ts
+++ medify/src/app/prescription-detail-card/prescription-detail-card.component.ts
@@ -8,6 +8,12 @@ import {
 } from '@angular/animations';
 import { Prescription } from '../Models/Prescription';
 import { UserdataService } from '../userdata.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { loopbackConnPatientsUrl } from '../loopbackConnectors';
+import { Appointment } from '../Models/Appointment';
+import { ToastServiceService } from '../toast-service.service';
+import { Med } from '../Models/Med';
+
 
 declare var $;
 @Component({
@@ -29,12 +35,20 @@ declare var $;
 })
 export class PrescriptionDetailCardComponent implements OnInit {
   public prescriptions: Prescription[]
+  loopbackPatientsUrl = loopbackConnPatientsUrl
+
   constructor(
-    private userData: UserdataService
+    private userData: UserdataService,
+    private http:HttpClient,
+    private toastService:ToastServiceService
   ) { }
 
   ngOnInit() {
     this.userData.currentPrescriptions.subscribe(pres => this.prescriptions = pres);
+    this.http.get(loopbackConnPatientsUrl.concat(localStorage.getItem('userEmail'))).subscribe(
+      data=>{
+        this.prescriptions = data['prescriptions']
+      })
     console.log(this.userData.currentPrescriptions)
 
   }
@@ -42,15 +56,46 @@ export class PrescriptionDetailCardComponent implements OnInit {
   changeDeliveryStatus(presIndex, medInPresIndex) {
     let pres = this.prescriptions[presIndex];
     let med = pres.meds[medInPresIndex];
-    if (med.delivered) {
-      this.prescriptions[presIndex].meds[medInPresIndex].delivered = false;
-    }
-    else {
-      this.prescriptions[presIndex].meds[medInPresIndex].delivered = true;
-    }
+    this.http.get(loopbackConnPatientsUrl.concat(localStorage.getItem('userEmail'))).subscribe(
+      data=>{
+      var prescriptions = data['prescriptions'];  
+      var editedPres = prescriptions[presIndex] as Prescription
+      var meds = editedPres.meds as Med[]
+      var editMed:any = meds[medInPresIndex]       
+      console.log("CLICKED CURRENT" + editMed.delivered) 
+      console.log("CLICKED NEW" + editMed.delivered) 
+      if (editMed.delivered == 'true') {
+        editMed.delivered = 'false'
+        data['prescriptions'] = prescriptions
+        this.http.put(this.loopbackPatientsUrl.concat(localStorage.getItem('userEmail')),data).subscribe(
+          data =>{
+            console.log("Success put")
+            this.prescriptions[presIndex].meds[medInPresIndex].delivered = false;
+            location.reload();
+            console.log(pres);
+            console.log(med);
+          }
+        )
 
-    console.log(pres);
-    console.log(med);
+      }
+      else if(editMed.delivered == 'false') {
+        editMed.delivered = 'true'
+        data['prescriptions'] = prescriptions
+        console.log(prescriptions)
+        this.http.put(this.loopbackPatientsUrl.concat(localStorage.getItem('userEmail')),data).subscribe(
+          data =>{
+            console.log("Success put")
+            this.prescriptions[presIndex].meds[medInPresIndex].delivered = true;
+            location.reload();
+            console.log(pres);
+            console.log(med);
+          }
+        )
+      }
+    
+
+    })
+  
 
 
     //location.reload();
