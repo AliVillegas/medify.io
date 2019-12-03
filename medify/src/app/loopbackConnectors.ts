28ca93955a61ff07ee92e38698cec039de42a6ae
AllBran <alibryan.villegas@gmail.com>
2019-12-03 10:40:32 -0600

    Modified connectors Loopback

--- medify/src/app/loopbackConnectors.ts
+++ medify/src/app/loopbackConnectors.ts
@@ -1,6 +1,13 @@
+const loopbackConnDoctorsUrl = 'http://LBS-Medify-1010050942.us-east-1.elb.amazonaws.com/doctors/'
+const loopbackConnPatientsUrl = 'http://LBS-Medify-1010050942.us-east-1.elb.amazonaws.com/patients/'
+const loopbackConnMedsUrl = 'http://LBS-Medify-1010050942.us-east-1.elb.amazonaws.com/meds/'
+const loopbackConnGendersEsUrl = 'http://LBS-Medify-1010050942.us-east-1.elb.amazonaws.com/es/genders/'
+const loopbackConnGendersEnUrl = 'http://LBS-Medify-1010050942.us-east-1.elb.amazonaws.com/en/genders/'
+/*
 const loopbackConnDoctorsUrl = 'http://localhost:3000/doctors/'
 const loopbackConnPatientsUrl = 'http://localhost:3000/patients/'
 const loopbackConnMedsUrl = 'http://localhost:3000/meds/'
 const loopbackConnGendersEsUrl = 'http://localhost:3000/es/genders/'
 const loopbackConnGendersEnUrl = 'http://localhost:3000/en/genders/'
+*/ 
 export { loopbackConnDoctorsUrl, loopbackConnPatientsUrl , loopbackConnMedsUrl,loopbackConnGendersEsUrl,loopbackConnGendersEnUrl};
